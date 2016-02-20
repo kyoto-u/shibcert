00b7539d66eb17d957447832ea30961e58f9c76d
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2016-02-20 14:34:14 +0900

    fix: cd (change directory)
    independent of environments
    cd `basename $0`/../../

--- lib/scripts/mail_processor_caller.sh
+++ lib/scripts/mail_processor_caller.sh
@@ -3,5 +3,6 @@
 PATH=/opt/rbenv/bin/:$PATH
 eval "$(rbenv init -)"
 
-cd /var/www/shibcert-prototype
-bundle exec rails runner lib/mail_processor.rb
+echo `dirname $0`/../../
+cd `dirname $0`/../../
+#bundle exec rails runner lib/mail_processor.rb
