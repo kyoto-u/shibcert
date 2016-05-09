bbe98de7fca692242b8d21460ca81074fece9663
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2016-05-09 16:29:21 +0900

    fix: update name and email attributes on every login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,10 @@ class SessionsController < ApplicationController
 #    raise request.env["omniauth.auth"].to_yaml
 
     auth = request.env['omniauth.auth']
+    logger.info("#{self}.#{__method__} auth=#{auth.inspect}")
     user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
+    logger.info("#{self}.#{__method__} user=#{user.inspect}")
+    update_user(user, auth)
     session[:user_id] = user.id
     redirect_to root_url, :notice => 'Signed in!'
   end
@@ -12,4 +15,12 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, :notice => 'Signed out!'
   end
+
+  private
+  def update_user(user, auth)
+    user[:name]  = auth[:info][:name]
+    user[:email] = auth[:info][:email]
+    user.save!
+  end
+
 end
