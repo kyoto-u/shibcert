a665249b9a303e228fcfefc30c0038e7d76fbadf
Noritoshi Atsumi <atsumi.noritoshi.5u@kyoto-u.ac.jp>
2018-01-23 16:41:30 +0900

    add jquery images and js

---
+++ batch/cert_counter.rb
@@ -0,0 +1,23 @@
+require 'date'
+
+class CertCounter
+  def self.execute
+    certs = Cert.where('state = ? or state = ?', Cert::State::NEW_GOT_SERIAL, Cert::State::RENEW_GOT_SERIAL)
+    client_certs = certs.where(purpose_type: Cert::PurposeType::CLIENT_AUTH_CERTIFICATE)
+    smime_certs =  certs.where(purpose_type: Cert::PurposeType::SMIME_CERTIFICATE)
+
+    today = Date.today
+    day = Date.new(2016,1,1)
+ 
+    while day <= today
+      day2 = day >> 1
+      cc = client_certs.where(created_at: [day...day2]).count
+      sc =  smime_certs.where(created_at: [day ..day2]).count
+#      printf("%s\t%s\t%d\t%d\n", day.strftime('%Y-%m-%d'), day2.strftime('%Y-%m-%d'), cc, sc)
+      printf("%s\t%d\t%d\n", day.strftime('%Y-%m'), cc, sc)
+      day = day2
+    end
+  end
+end
+
+CertCounter.execute
