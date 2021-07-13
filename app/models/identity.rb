5e55a081ae0ab61463bf09b2a7818e0be73e399f
Komura Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2021-07-13 16:19:03 +0900

    modify: use uid as auth_key in OmniauthIdentity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,6 +4,8 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   has_secure_password
 
+  auth_key :uid
+
   scope :by_email_like, lambda {|email|
     where('email LIKE :value', { value: "#{sanitize_sql_like(email)}%"})
   }
