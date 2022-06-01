8f6ced82e39c4e0241cadc08a02980e869e90b42
Komura Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2022-06-01 16:33:14 +0900

    TEST remove 'should update cert' and 'should destroy cert'

--- test/controllers/certs_controller_test.rb
+++ test/controllers/certs_controller_test.rb
@@ -21,17 +21,4 @@ class CertsControllerTest < ActionController::TestCase
     post :edit_memo_remote, params:{id: certs(:certs_one).id, cert: {memo: "change memo"}}
     assert_response :redirect
   end
-
-  test "should update cert" do
-    patch :update, id: @cert, cert: { expire_at: @cert.expire_at, get_at: @cert.get_at, memo: @cert.memo, pin: @cert.pin, pin_get_at: @cert.pin_get_at, user_id: @cert.user_id }
-    assert_redirected_to cert_path(assigns(:cert))
-  end
-
-  test "should destroy cert" do
-    assert_difference('Cert.count', -1) do
-      delete :destroy, id: @cert
-    end
-
-    assert_redirected_to certs_path
-  end
 end
