d67f9085a56d09eaa2ca3d43b06eb33f69d1eab2
Komura Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2022-06-01 16:21:19 +0900

    TEST remove Cert.create

--- test/controllers/certs_controller_test.rb
+++ test/controllers/certs_controller_test.rb
@@ -12,14 +12,6 @@ class CertsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:certs)
   end
 
-  test "should create cert" do
-    assert_difference('Cert.count') do
-      post :create, cert: { expire_at: @cert.expire_at, get_at: @cert.get_at, memo: @cert.memo, pin: @cert.pin, pin_get_at: @cert.pin_get_at, user_id: @cert.user_id }
-    end
-
-    assert_redirected_to cert_path(assigns(:cert))
-  end
-
   test "should show cert" do
     get :show, id: @cert
     assert_response :success
